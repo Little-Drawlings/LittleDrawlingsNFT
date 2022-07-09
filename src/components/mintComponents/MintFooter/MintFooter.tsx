4f41b5999928a7e32d35a3649affecd983637893
kusergiy <kustovskuysergiy@gmail.com>
2022-07-09 09:38:45 +0300

    foter animation

--- src/components/mintComponents/MintFooter/MintFooter.tsx
+++ src/components/mintComponents/MintFooter/MintFooter.tsx
@@ -1,26 +1,30 @@
 import cn from 'classnames';
-import { ParallaxBanner } from 'react-scroll-parallax';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 import icons from '../../../constants/icons';
 
 import styles from './MintFooter.module.scss';
 
 const MintFooter: React.FC = () => {
+    const [ref, view] = useInView({
+        threshold: 0.5,
+        triggerOnce: true,
+    });
+
+    const variants = {
+        visible: { top: '-82%' }
+    };
+
     return (
-        <div className={cn('mint-wrapper', 'mint-height_auto', styles.mint_footer)}>
-            <img src={icons.ParallaxPlan1} alt="Parallax" />
-            {/* <ParallaxBanner
-                layers={[{
-                    image: icons.ParallaxPlan1, speed: -15,
-                    shouldAlwaysCompleteAnimation: true,
-                    children: (
-                        <div className="">
-                            <img src={icons.ParallaxPlan2} alt="Parallax2" />
-                        </div>
-                    ),
-                }]}
-                className={cn("aspect-[2/1]", styles.parallax_img)}
-            /> */}
+        <div className={cn('mint-wrapper', 'mint-height_auto', styles.mint_footer)} >
+            <motion.div className={styles.parallax_bg_wrap} animate={view ? 'visible' : 'hidden'}
+                ref={ref}
+                transition={{ duration: 2, ease: 'easeOut' }}
+                variants={variants}>
+                <img className={styles.parallax_bg} src={icons.ParallaxPlan2} alt="Parallax2" />
+            </motion.div>
+            <img className={styles.footer_bg} src={icons.ParallaxPlan1} alt="Parallax1" />
         </div>
     )
 }
