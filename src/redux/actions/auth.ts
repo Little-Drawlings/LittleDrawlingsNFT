c987f26c7972e27732625cecc092de3ae71e47b3
kusergiy <kustovskuysergiy@gmail.com>
2022-08-26 15:54:48 +0300

    add new wallet + fixed bugs

--- src/redux/actions/auth.ts
+++ src/redux/actions/auth.ts
@@ -1,4 +1,5 @@
 import { ethers } from "ethers";
+import RLogin from "@rsksmart/rlogin";
 
 import API, { deleteToken, setToken } from '../../api';
 import types from "../reducers/auth/types";
@@ -22,58 +23,76 @@ export const signOut = () => {
 export const signInMetamask = () => async (dispatch: (arg0: { type: string; data: MetaMaskData | boolean }) => void) => {
     const w: any = window;
 
-    const provider = new ethers.providers.Web3Provider(w.ethereum);
-    const { chainId } = await provider.getNetwork()
-    if (chainId !== 31) {
-        try {
-            await w.ethereum.request({
-                method: 'wallet_switchEthereumChain',
-                params: [{ chainId: "0x1f" }],
-            })
-        }
-        catch {
-            await w.ethereum.request({
-                jsonrpc: '2.0',
-                method: 'wallet_addEthereumChain',
-                params: [
-                    {
-                        chainId: "0x1f",
-                        rpcUrls: ['https://public-node.testnet.rsk.co'],
-                        nativeCurrency: {
-                            name: 'tRBTC',
-                            symbol: 'tRBTC',
-                            decimals: 18
-                        }
-                    }
-                ]
-            })
-        }
+    const infoOptions = {
+        30: { addressBaseURL: 'https://explorer.rsk.co/address/' },
+        31: { addressBaseURL: 'https://explorer.testnet.rsk.co/address/' }
     }
 
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    const coinbase = await signer.getAddress();
-    const account = coinbase.toLowerCase();
-    const nonce = await getNonce(account);
-    const signature = await signer.signMessage(`I am signing my one-time nonce: ${nonce}`)
+    const rLogin = new RLogin({
+        infoOptions
+    });
 
-    if (!signature) {
-        signOut();
-        return null;
-    }
-    const data = { account, signature, chainId };
-    dispatch(setLoading(true));
-    return API.post('/auth/login-metamask', data)
-        .then(async (response) => {
-            if (response?.status) {
-                setToken(response.data?.data.token);
-                dispatch({
-                    type: types.SIGN_IN_METAMASK,
-                    data: response.data?.data
-                })
+    return rLogin.connect()
+        .then(async ({ provider }) => {
+
+            const ethersProvider = new ethers.providers.Web3Provider(provider);
+
+            const isMetamask = ethersProvider.connection?.url === 'metamask'
+
+
+            const { chainId } = await ethersProvider.getNetwork();
+
+            if (chainId !== 31 && chainId !== 30 && isMetamask) {
+                try {
+                    await w.ethereum.request({
+                        method: 'wallet_switchEthereumChain',
+                        params: [{ chainId: "0x1f" }],
+                    })
+                }
+                catch {
+                    await w.ethereum.request({
+                        jsonrpc: '2.0',
+                        method: 'wallet_addEthereumChain',
+                        params: [
+                            {
+                                chainId: "0x1f",
+                                rpcUrls: ['https://public-node.testnet.rsk.co'],
+                                nativeCurrency: {
+                                    name: 'tRBTC',
+                                    symbol: 'tRBTC',
+                                    decimals: 18
+                                }
+                            }
+                        ]
+                    })
+                }
             }
+
+            await ethersProvider.send("eth_requestAccounts", []);
+            const signer = ethersProvider.getSigner();
+            const coinbase = await signer.getAddress();
+            const account = coinbase.toLowerCase();
+            const nonce = await getNonce(account);
+            const signature = await signer.signMessage(`I am signing my one-time nonce: ${nonce}`)
+
+            if (!signature) {
+                signOut();
+                return null;
+            }
+            const data = { account, signature, chainId };
+            dispatch(setLoading(true));
+            return API.post('/auth/login-metamask', data)
+                .then(async (response) => {
+                    if (response?.status) {
+                        setToken(response.data?.data.token);
+                        dispatch({
+                            type: types.SIGN_IN_METAMASK,
+                            data: response.data?.data
+                        })
+                    }
+                })
+                .catch((error) => {
+                    throw error;
+                }).finally(() => dispatch(setLoading(false)))
         })
-        .catch((error) => {
-            throw error;
-        }).finally(() => dispatch(setLoading(false)))
 };
