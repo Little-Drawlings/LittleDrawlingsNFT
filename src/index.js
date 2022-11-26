96105b70d638250cb3ee79e259ad5824476681fa
vashkiv <vashkiv7@gmail.cim>
2022-11-26 16:23:19 +0200

    downgrade react version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 import {Provider} from "./store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
   <React.StrictMode>
       <Router>
           <Provider>
               <App/>
           </Provider>
       </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+    document.getElementById('root')
 );
